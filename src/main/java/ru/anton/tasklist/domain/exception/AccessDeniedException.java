3ca6e37867ef7d1c5d6aac411ec1e136ff7f2f69
Anton Kolchunov <kolcunovanton@gmail.com>
2023-05-28 16:09:53 +0300

    Initial commit

---
+++ src/main/java/ru/anton/tasklist/domain/exception/AccessDeniedException.java
@@ -0,0 +1,7 @@
+package ru.anton.tasklist.domain.exception;
+
+public class AccessDeniedException extends RuntimeException{
+    public AccessDeniedException() {
+        super();
+    }
+}
---
+++ src/main/java/ru/anton/tasklist/domain/exception/ResourceMappingException.java
@@ -0,0 +1,7 @@
+package ru.anton.tasklist.domain.exception;
+
+public class ResourceMappingException extends RuntimeException{
+    public ResourceMappingException(String message) {
+        super(message);
+    }
+}
